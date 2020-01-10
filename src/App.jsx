230d68805e7a172fe281f637811b0aef06032d03
Arjan Einbu <arjan@einbu.no>
2020-01-10 17:42:56 +0100

    Added some navigation urls for easier testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, { /*useContext*/ } from 'react'
 import './App.css'
 import { useParams } from "react-router-dom"
-import { StateRouter, RouteView} from "./StateRouter"
+import { StateRouter, RouteView } from "./StateRouter"
+
 
 const Batch = () => <h2>Batch frame</h2>
 
-const WelcomeToProflow = () => <h2>Welcome to Proflow</h2>
+const WelcomeToProflow = () => <>
+    <h2>Welcome to Proflow</h2>
+    <ul>
+        <li><a href="/tenant/alfa">Alfa</a></li>
+        <li><a href="/tenant/beta">Beta</a></li>
+        <li><a href="/tenant/gamma">Gamma</a></li>
+        <li><a href="/tenant/delta">Delta</a></li>
+    </ul>
+</>
+
 
 const Dashboard = () => <h2>Dashboard frame</h2>
 
@@ -22,23 +32,33 @@ const Material = () => <h2>Material frame</h2>
 
 const Order = () => {
     const params = useParams()
-    // const match = useRouteMatch()
+    console.log("-", params)
     return <>
         <h2>Order frame for order number: {params.orderNumber}</h2>
         <RouteView>
-            <p>Fallback content for invalid url at order level</p>
+            <ul>
+                TODO: params.tenantId is not available here! Should be able to default it!!!
+                <li><a href={`/tenant/${params.tenantId}/order/${params.orderNumber}/operation/10`}>Operation 10</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/${params.orderNumber}/operation/20`}>Operation 20</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/${params.orderNumber}/operation/30`}>Operation 30</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/${params.orderNumber}/operation/40`}>Operation 40</a></li>
+            </ul>
         </RouteView>
     </>
 }
-
-
 
 const Tenant = () => {
     const params = useParams()
     return <>
         <h2>Tenant Frame for tenant id: {params.tenantId}</h2>
         <RouteView>
-            <p>Fallback content for invalid url at second level</p>
+            <ul>
+                <li><a href={`/tenant/${params.tenantId}/dashboard`}>Dashboard</a></li>
+                <li><a href={`/tenant/${params.tenantId}/batch`}>Batch</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/100`}>Order 100</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/101`}>Order 101</a></li>
+                <li><a href={`/tenant/${params.tenantId}/order/102`}>Order 102</a></li>
+            </ul>
         </RouteView>
     </>
 }
@@ -87,9 +107,6 @@ const mainRoutes = [
         ]
     }
 ]
-
-
-
 
 export const App = () => {
     return <>
